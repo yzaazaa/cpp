743b7a15414745717fd0789d22f3df6b120c4224
Yassine Zaazaa <yzaazaa@e1r7p3.1337.ma>
2024-03-20 00:27:05 +0000

    ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -12,13 +12,13 @@ static void	replace(char **av, std::string str)
 	std::ofstream	file_stream;
 	int				pos;
 
-	file_stream.open(std:: string (av[1]) + ".replace");
+	file_stream.open(std:: string (av[1]) + "_replace");
 	if (file_stream.fail())
-		failed(std::string (av[1]) + ".replace");
+		failed(std::string (av[1]) + "_replace");
 	for (int i = 0; i < (int)str.length(); i++)
 	{
 		pos = str.find(av[2], i);
-		if (pos != -1 && pos == i)
+		if (pos != -1 && pos == i && !std::string (av[2]).empty() && !std::string (av[3]).empty())
 		{
 			file_stream << av[3];
 			i += std::string(av[2]).length() - 1;
@@ -41,8 +41,9 @@ int	main(int ac, char **av)
 	if (file_stream.fail())
 		failed(std::string (av[1]));
 	std::string		str;
-	while(!file_stream.eof() && !getline(file_stream, str).eof())
-		;
+	char			c;
+	while(!file_stream.eof() && file_stream >> std::noskipws >> c)
+		str += c;
 	file_stream.close();
 	replace(av, str);
 	return (0);
