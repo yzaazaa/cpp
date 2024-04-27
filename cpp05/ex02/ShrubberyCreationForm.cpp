15f200d1f50fecdf2c0f8cf9381b9a030e5bc747
yzaazaa <yassine.zaazaa@outlook.com>
2024-04-27 10:13:34 +0100

    shrubbery

--- cpp05/ex02/ShrubberyCreationForm.cpp
+++
@@ -1,7 +0,0 @@
-#include "ShrubberyCreationForm.hpp"
-
-ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137), target("default") {}
-
-ShrubberyCreationForm::ShrubberyCreationForm(std::string const &target) : AForm("ShrubberyCreationForm", 145, 137), target(target) {}
-
-ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137), target(target) {}
--- cpp05/ex02/ShrubberyCreationForm.hpp
+++ cpp05/ex02/ShrubberyCreationForm.hpp
@@ -8,11 +8,12 @@ class	ShrubberyCreationForm : public AForm
 private:
 	std::string	const	target;
 public:
-	virtual void	execute(Bureaucrat const &executor);
+	virtual void		execute(Bureaucrat const &executor);
+	std::string const	&getTarget();
 
 	ShrubberyCreationForm();
-	ShrubberyCreationForm(std::string const target);
-	ShrubberyCreationForm(ShrubberyCreationForm const &shrubberyCreationForm);
+	ShrubberyCreationForm(std::string const &target);
+	ShrubberyCreationForm(ShrubberyCreationForm const &form);
 	ShrubberyCreationForm	&operator=(ShrubberyCreationForm const &rhs);
 	~ShrubberyCreationForm();
 };
